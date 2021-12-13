8f13d8e5299f6ae621f9fe0e405adc961fdde887
ejero <rositaemakpo@gmail.com>
2021-12-12 17:12:17 -0800

    add data set and update app.py file

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
+import inline as inline
+import matplotlib
 import requests
-import sys
+import os
 from pprint import pprint
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from seaborn import regression
+
 
 
 # API used: https://exchangerate.host/#/#docs
 
-print("ENTER THREE DIGIT CURRENCY SYMBOL WHEN PROMPTED ie USD or EUR")
+print("ENTER THREE LETTER CURRENCY CODE WHEN PROMPTED ie USD or EUR")
 print("")
-currency_symbol1 = input("From currency symbol: ")
-currency_symbol2 = input("To currency symbol: ")
+currency_code1 = input("From currency code: ")
+currency_code2 = input("To currency code: ")
 amount = input("Amount: ")
 url = 'https://api.exchangerate.host/convert'
 response = requests.get(url, params={
-    "from": currency_symbol1,
-    "to": currency_symbol2,
+    "from": currency_code1,
+    "to": currency_code2,
     "amount": amount,
     "places": 2
 })
 data = response.json()['result']
-print("result: ",end="")
-pprint(data)
+print("Result: ",end="")
+print(data)
+print("")
+print("Historical data")
+
+currency_data = pd.read_csv("GBPUSD=X-monthly.csv")
+print(currency_data.head())
+
+
+# plot data from CSV file
+axis = sns.lineplot(x="Date", y="Close", data =currency_data)
+plt.xticks(rotation=45)
+plt.show()
+
